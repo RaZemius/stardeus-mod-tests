ebeba3a40eb02fc0ea90cef29ca57689e9b14b38
RaZemius <df260704@gmail.com>
2023-01-28 09:18:00 +0300

    hm

--- .vscode/build.bat
+++ .vscode/build.bat
@@ -10,10 +10,10 @@ REM build dll
 dotnet build .vscode
 rem moving file
 DEL "C:\Users\fadee\AppData\LocalLow\Kodo Linija\Stardeus\Mods\newmod" /S /q
-xcopy .\Libraries "C:\Users\fadee\AppData\LocalLow\Kodo Linija\Stardeus\Mods\newmod\Libraries\"
+xcopy .\Libraries "C:\Users\fadee\AppData\LocalLow\Kodo Linija\Stardeus\Mods\newmod\Libraries\" /e
 copy ModInfo.json "C:\Users\fadee\AppData\LocalLow\Kodo Linija\Stardeus\Mods\newmod\"
-xcopy .\Graphics "C:\Users\fadee\AppData\LocalLow\Kodo Linija\Stardeus\Mods\newmod\Graphics\"
-xcopy .\translations "C:\Users\fadee\AppData\LocalLow\Kodo Linija\Stardeus\Mods\newmod\translations\"
-xcopy .\Definitions "C:\Users\fadee\AppData\LocalLow\Kodo Linija\Stardeus\Mods\newmod\Defenitions\"
-xcopy .\config "C:\Users\fadee\AppData\LocalLow\Kodo Linija\Stardeus\Mods\newmod\config\"
+xcopy .\Graphics "C:\Users\fadee\AppData\LocalLow\Kodo Linija\Stardeus\Mods\newmod\Graphics\" /e
+xcopy .\translations "C:\Users\fadee\AppData\LocalLow\Kodo Linija\Stardeus\Mods\newmod\translations\" /e
+xcopy .\Definitions "C:\Users\fadee\AppData\LocalLow\Kodo Linija\Stardeus\Mods\newmod\Defenitions\" /e 
+xcopy .\config "C:\Users\fadee\AppData\LocalLow\Kodo Linija\Stardeus\Mods\newmod\config\" /e
 rem start "C:\Program Files (x86)\Steam\steamapps\common\Stardeus\Stardeus.exe"
